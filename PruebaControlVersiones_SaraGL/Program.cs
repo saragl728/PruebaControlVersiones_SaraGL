11622be0ad1ae1ca6642f8bce1012efe9b3b7660
saragl728 <saragl728@gmail.com>
2023-03-09 12:04:06 +0100

    Se añade un método para la entrada

--- PruebaControlVersiones_SaraGL/Program.cs
+++ PruebaControlVersiones_SaraGL/Program.cs
@@ -6,8 +6,17 @@
         {
             Modelo prueba;
             prueba = new Modelo();
+            prueba.Nombre = MeterCadena("Introducir el nombre");
+            prueba.Apellidos = MeterCadena("Introducir los apellidos");
 
             Console.WriteLine("Sara");
+        }
+        static string MeterCadena(string mensaje)
+        {
+            string cadena;
+            Console.WriteLine($"{mensaje}");
+            cadena = Console.ReadLine();
+            return cadena;
         }
     }
 }
